b34003bcd42055c5695f002405654e7445587aea
Ali Saim <Ali@Alis-MacBook-Air.local>
2016-10-22 16:45:03 -0400

    Internal Documentation

--- MidTerm - StarWars/Assets/_Scripts/PlayerCollider.cs
+++ MidTerm - StarWars/Assets/_Scripts/PlayerCollider.cs
@@ -1,4 +1,14 @@
-﻿using UnityEngine;
+﻿
+/*Student Name: Ali Saim
+ * Student Number: 300759480
+ * Course: COMP 305- Section: (Combined)
+ * Program Name: Gamee Programming 1
+ * Date last Modified: October 22, 2016 
+ * 
+ * */
+
+
+using UnityEngine;
 using System.Collections;
 
 public class PlayerCollider : MonoBehaviour {
